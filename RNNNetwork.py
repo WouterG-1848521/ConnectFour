0e66e1bd68d8fd6bc46db45195f5819470411229
Wouter Grootjans <wouter.grootjans@student.uhasselt.be>
2022-06-06 10:10:18 +0200

    Working RNN, but nog good results'

--- RNNNetwork.py
+++ RNNNetwork.py
@@ -47,36 +47,59 @@ train_labels = np.array(train_labels)
 test_features = np.array(test_features)
 test_labels = np.array(test_labels)
 
+# prepare data for training a recurrent neural network
+# test 1
+# dim_1 = 10
+# dim_2 = int(len(train_features) / dim_1)
+# dim_3 = 42
+
+# leftover = len(train_features) % dim_1
+# train_features = train_features[0: len(train_features) - leftover]
+# train_features = train_features.reshape(dim_1, dim_2, dim_3)
+
+# dim_2 = int(len(test_features) / dim_1)
+# leftover = len(test_features) % dim_1
+# test_features = test_features[0: len(test_features) - leftover]
+# test_features = test_features.reshape(dim_1, dim_2, dim_3)
+
+# test 2
 # for i in range(len(train_features)):
 #     np.insert(train_features, 0, i)
 # for i in range(len(test_features)):
 #     test_features[i] = [i, test_features[i]]
 # inputs = np.random.random([32, 10, 8]).astype(np.float32)
 
+# test 3
+test_features = test_features.reshape(test_features.shape[0], test_features.shape[1], 1)
+train_features = train_features.reshape(train_features.shape[0], train_features.shape[1], 1)
+
 print("Done loading data ",train_features.shape, " ", train_labels.shape, " ", test_features.shape, " ", test_labels.shape)
 print("\n\n\n")
 
 # test a Recurrent Neural Network
 
+regularizer = tf.keras.regularizers.OrthogonalRegularizer(factor=0.01, mode="columns")
+
 model = tf.keras.Sequential()
 # model.add(tf.keras.layers.Flatten()) 
-model.add(tf.keras.layers.SimpleRNN(64, input_shape=(None, 42)))
+model.add(tf.keras.layers.SimpleRNN(512, input_shape=(42, 1), activation='relu', kernel_regularizer=regularizer))
 model.add(tf.keras.layers.BatchNormalization())
-model.add(tf.keras.layers.Dense(10))
-# print(model.summary())
+model.add(tf.keras.layers.Dense(7, activation=tf.nn.softmax))
 
-# quit()
 
 model.compile(optimizer='adam',
-              loss='sparse_categorical_crossentropy',
+              loss='categorical_crossentropy',
               metrics=['accuracy'])
-# quit()
-history = model.fit(train_features, tf.keras.utils.to_categorical(train_labels), epochs=5)
+print(model.summary())
+
+early_stopping_monitor = tf.keras.callbacks.EarlyStopping(monitor="accuracy",patience=10, min_delta=0.001)
+early_stopping_monitor2 = tf.keras.callbacks.EarlyStopping(monitor="loss",patience=10, min_delta=0.001)
+history = model.fit(train_features, tf.keras.utils.to_categorical(train_labels), epochs=100,  callbacks=[early_stopping_monitor,early_stopping_monitor2])
 
 print(history.history)
 
 print("Evaluate on test data")
-results = model.evaluate(test_features, test_labels, batch_size=128)
+results = model.evaluate(test_features,  tf.keras.utils.to_categorical(test_labels), batch_size=128)
 print("test loss, test acc:", results)
 
 
